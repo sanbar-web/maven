272f7c14964f041170a28fc5ba19ee2617b3ea4b
sanbar-web <63513726+sanbar-web@users.noreply.github.com>
2020-04-18 21:08:27 +0530

    text

---
+++ src/main/java/com/sample/maven/samplemaven/App.java
@@ -0,0 +1,13 @@
+package com.sample.maven.samplemaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello hi World!" );
+    }
+}
